8cb205ffb7dd31709186117afd50bd67832bf7f0
hanzalah-3000 <140268646+hanzalah-3000@users.noreply.github.com>
2023-07-23 00:22:03 +0500

    Adding Distraction

--- Assets/Scripts/Distraction.cs
+++ Assets/Scripts/Distraction.cs
@@ -1,25 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditorInternal;
 using UnityEngine;
-using Unity.AI;
 using UnityEngine.AI;
 
-public class Controller : MonoBehaviour
+public class Distraction : MonoBehaviour
 {
-    [SerializeField] GameObject enemy;
     [SerializeField] NavMeshAgent player;
-    
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<NavMeshAgent>();
+        player= GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        player.SetDestination(enemy.transform.position);
+        
     }
-    
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision");
+    }
 }
