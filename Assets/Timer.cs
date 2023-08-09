1c7f45d13fa49ba5c908b8fa8a5fc897767195ec
hanzalah-3000 <140268646+hanzalah-3000@users.noreply.github.com>
2023-08-10 00:03:54 +0500

    Timer Added

---
+++ Assets/Timer.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timer;
+    public TMP_Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 15.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            
+            timerText.text = timer.ToString("F0");
+        }
+        else {
+            Debug.Log("Timer Completed");
+        }
+    }
+}
