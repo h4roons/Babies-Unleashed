a6f3c0185697d853ab92533e3b36b1ce70eeb557
hanzalah-3000 <140268646+hanzalah-3000@users.noreply.github.com>
2023-08-27 00:17:17 +0500

    Decorating and shit
    Jheeleya aniha muhabtaan na paa

--- Assets/Scripts/PhoneManager.cs
+++ Assets/Scripts/PhoneManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RubixManager : MonoBehaviour
+public class PhoneManager : MonoBehaviour
 {
     [SerializeField] GameObject touchManager;
     
